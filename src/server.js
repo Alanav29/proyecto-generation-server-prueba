a0b33e4659e8f467a5c43a73c7a3abcbf5a9e153
Alanav29 <josephalannavarro@gmail.com>
2023-07-13 02:57:08 -0600

    Se incio modelo de usuario

--- src/server.js
+++ src/server.js
@@ -14,10 +14,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/movies", require("./routes/moviesRoutes"));
-
 app.use("/api/users", require("./routes/userRoutes"));
 
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`.green));
