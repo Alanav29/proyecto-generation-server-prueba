e952c74e63bba068b629d93cd815bd82b91f7eb0
Alanav29 <josephalannavarro@gmail.com>
2023-07-23 00:48:57 -0600

    Se hizo pull de main

--- src/controllers/commentsControllers.js
+++ src/controllers/commentsControllers.js
@@ -1,65 +1,67 @@
-const CommentModel = require('../models/commentModel')
-const asyncHandler = require('express-async-handler')
+const CommentModel = require("../models/commentModel");
+const asyncHandler = require("express-async-handler");
 
-const createComment = asyncHandler( async (req,res) => {
+const createComment = asyncHandler(async (req, res) => {
+	/**postType debe ser un string 'LostPet','ShelteredPet','AdoptionPet' */
+	const { text, postId, postType } = req.body;
 
-    /**postType debe ser un string 'LostPet','ShelteredPet','AdoptionPet' */
-    const {text, postId, postType} = req.body
+	if (!text) {
+		res.status(400);
+		throw new Error("Some text is required to post a comment");
+	}
 
-    if (!text) {
-        res.status(400)
-		throw new Error("Some text is required to post a comment")
-    }
+	const comment = await CommentModel.create({
+		user_id: req.user.id,
+		text: text,
+		post: postId,
+		postType: postType,
+	});
 
-    const comment = await CommentModel.create({
-        user_id: req.user.id,
-        text: text,
-        post: postId,
-        postType: postType
-    })
+	console.log(
+		`${req.user.name} made a new comment on the following ${postType} post: ${postId}`
+	);
+	res.status(200).json({ comment });
+});
 
-    console.log(`${req.user.name} made a new comment on the following ${postType} post: ${postId}`)
-    res.status(200).json({comment})
+const editComment = asyncHandler(async (req, res) => {
+	const comment = await CommentModel.findById(req.params.id);
+	if (!comment) {
+		res.status(400);
+		throw new Error("Comment not found");
+	}
 
-} )
+	const commentUpdated = await CommentModel.findByIdAndUpdate(
+		req.params.id,
+		req.body,
+		{ new: true }
+	);
+	res.status(200).json(commentUpdated);
+});
 
-const editComment = asyncHandler ( async (req, res) => {
-    const comment = await CommentModel.findById(req.params.id)
-    if(!comment){
-        res.status(400)
-        throw new Error('Comment not found')
-    }
+const deleteComment = asyncHandler(async (req, res) => {
+	const comment = await CommentModel.findById(req.params.id);
+	if (!comment) {
+		res.status(400);
+		throw new Error("Comment not found");
+	}
+	await comment.deleteOne();
+	res.status(200).json({ id: req.params.id });
+});
 
-    const commentUpdated = await CommentModel.findByIdAndUpdate(req.params.id, req.body, {new: true})
-    res.status(200).json(commentUpdated)
-})
+const getPostComments = asyncHandler(async (req, res) => {
+	const comments = await CommentModel.find();
+	let filteredComments = comments.filter((comment) => {
+		return comment.post.valueOf() === req.params.id;
+	});
+	console.log(`Post Id: ${req.params.id}`);
+	console.log(filteredComments);
 
-const deleteComment = asyncHandler( async(req,res)=> {
-    const comment = await CommentModel.findById(req.params.id)
-    if (!comment){
-        res.status(400)
-        throw new Error('Comment not found')
-    }
-    await comment.deleteOne()
-    res.status(200).json({id: req.params.id})
-})
-
-const getPostComments = asyncHandler( async(req,res)=>{
-   
-    const comments = await CommentModel.find()
-    let filteredComments = comments.filter((comment) => {
-        return comment.post.valueOf() === req.params.id
-    })
-    console.log(`Post Id: ${req.params.id}`)
-    console.log(filteredComments)
-
-    res.status(200).json(filteredComments)
-    })
-
+	res.status(200).json(filteredComments);
+});
 
 module.exports = {
-   createComment,
-   editComment,
-   deleteComment,
-   getPostComments
-}
+	createComment,
+	editComment,
+	deleteComment,
+	getPostComments,
+};
