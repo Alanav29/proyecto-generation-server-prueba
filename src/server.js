f62345f449f5e66ee0e48f544addd0ef6562dfc2
Alanav29 <josephalannavarro@gmail.com>
2023-08-03 04:45:47 -0600

    se volvio a agregar express upload

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
+const fileUpload = require("express-fileupload");
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -13,6 +14,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+	fileUpload({
+		useTempFiles: true,
+		tempFileDir: "./uploads",
+	})
+);
 
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/lostPets", require("./routes/lostPetRoutes"));
